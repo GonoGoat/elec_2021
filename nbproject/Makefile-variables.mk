7d1b803d568fc8b0550dcc45e41e65a39d4f4214
DamienBelga <47674305+DamienBelga@users.noreply.github.com>
2021-12-09 16:12:28 +0100

    Full code
    Projet complet pour le 7 segments

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Projet_7Segments.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Projet_7Segments.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=projet7segments.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/projet7segments.x.tar
