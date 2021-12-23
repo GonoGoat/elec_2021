119135d82b5f5be1176678733f93322d5173346a
GonoGoat <gauth.vrsgn@outlook.be>
2021-12-23 16:28:46 +0100

    Application alim

--- Alim.X/main.c
+++ Alim.X/main.c
@@ -20,6 +20,28 @@
 #define PCF3_W 0b01000000   // Adresse en ?criture du PCF8574 3
 #define PCF4_W 0b01001110   // Adresse en ?criture du PCF8574 4
 
+#include "main.h"
+
+// PREFIXES
+#define PRE_SET_VALUE ":01060015"
+
+// ENABLE
+#define ENABLE_REMOTE_MODE ":010601000001F7"
+
+// CONFIGURE 
+#define ENABLE_VOLTAGE ":010600160000E3"
+#define ENABLE_CURRENT ":010600160010D3"
+#define ENABLE_POWER ":010600160020C3"
+
+// READING
+#define READ_VOLTAGE ":010300100001EB"
+#define READ_CURRENT ":010300110001EA"
+#define READ_POWER ":010300120001E9"
+
+// COMMANDS
+#define POWER_ON ":010600170011D1"
+#define POWER_OFF ":010600170000E2"
+
 
 /* PORTAbits.RA0 = Bouton Up
 * PORTAbits.RA1 = Bouton OK
@@ -28,6 +50,174 @@
 * PORTBbits.RB5 = LED_1
 * PORTBbits.RB4 = LED_2
 */
+
+void to_upper (char * str) {
+  for (int i = 0; str[i]; i++) {
+    char c = str[i];
+    if (c >= 97 && c <= 122) {
+      c -= 32;
+    }
+    str[i] = c;
+  }
+}
+
+char ascii_hex_to_decimal(char letter) {
+    char num = 0;
+    num = num << 4;
+    num = (letter<='9')?(letter-'0'):(letter-'a'+10);
+    num = (num & 0b00001111);
+    return num;
+}
+
+void decimal_to_ascii_hex(int decimal, char * buffer) {
+    sprintf(buffer, "%x", decimal);
+}
+
+void send_tram(char * tram) {
+    while(*tram != '\0') {
+        while (!TXIF);
+        TXREG = *tram;
+        tram++;
+    }
+    while (!TXIF);
+    TXREG = '\r';
+    while (!TXIF);
+    TXREG = '\n';
+}
+
+void receive_tram(char * buffer) { 
+    RCSTAbits.CREN = 1;
+    if(OERR) {
+        //If error, set bit of reading to 1.
+        RCSTAbits.CREN = 0;
+        RCSTAbits.CREN = 1;
+    }
+    
+    while(!RCIF);
+    while(RCREG != '\r') {
+        //Wait until the reception buffer is empty.
+        while(!RCIF);
+        *buffer = RCREG;
+        buffer++;
+    }
+}
+
+int chksum_calculation (const char * chain, char * buffer) {
+  int i, j, chksum = 0;
+  char chksum_str[3] = "";
+
+  strcpy(buffer, chain);
+  
+  for (i = 1; chain[i]; i+=2) {
+    int s = ascii_hex_to_decimal(chain[i]) * 16 + ascii_hex_to_decimal(chain[i+1]);
+    chksum += s;
+  }
+
+  printf("%d %x\n", chksum, chksum);
+  chksum = (chksum & 0b11111111);
+
+  decimal_to_ascii_hex(256-chksum, chksum_str);
+  strcat(buffer, chksum_str);
+
+  return chksum;
+}
+
+void set_value (char tram[30], int val) {
+    char asciihex[10] = "";
+    sprintf(asciihex, "%04x", val);
+    strcat(tram, asciihex);
+}
+
+void set_voltage (int voltage) {
+    // Enable remote mode
+    // Send UART
+    send_tram(ENABLE_REMOTE_MODE);
+    // Enable voltage mode
+    // Send UART
+    send_tram(ENABLE_VOLTAGE);
+    // Set V value
+    char new_tram3[30] = "";
+    char tram[30] = PRE_SET_VALUE;
+    set_value(tram, voltage);
+    // Checksum
+    chksum_calculation(tram, new_tram3);
+    to_upper(new_tram3);
+    // Send UART
+    send_tram(new_tram3);
+    printf("%s\n%s\n%s\n", ENABLE_REMOTE_MODE, ENABLE_VOLTAGE, new_tram3);
+}
+
+void set_current (int current) {
+    // Enable remote mode
+    // Send UART
+    send_tram(ENABLE_REMOTE_MODE);
+    // Enable current mode
+    // Send UART
+    send_tram(ENABLE_CURRENT);
+    // Set A value
+    char new_tram3[30] = "";
+    char tram[30] = PRE_SET_VALUE;
+    set_value(tram, current);
+    // Checksum
+    chksum_calculation(tram, new_tram3);
+    to_upper(new_tram3);
+    // Send UART
+    send_tram(new_tram3);
+    printf("%s\n%s\n%s\n", ENABLE_REMOTE_MODE, ENABLE_CURRENT, new_tram3);
+}
+
+void set_power (int power) {
+    // Enable remote mode
+    // Send UART
+    send_tram(ENABLE_REMOTE_MODE);
+    // Enable power mode
+    // Send UART
+    send_tram(ENABLE_POWER);
+    // Set W value
+    char new_tram3[30] = "";
+    char tram[30] = PRE_SET_VALUE;
+    set_value(tram, power);
+    // Checksum
+    chksum_calculation(tram, new_tram3);
+    to_upper(new_tram3);
+    // Send UART
+    send_tram(new_tram3);
+    printf("%s\n%s\n%s\n", ENABLE_REMOTE_MODE, ENABLE_POWER, new_tram3);
+}
+
+void turn_power_on (void) {
+  send_tram(POWER_ON);
+}
+
+void turn_power_off (void) {
+  send_tram(POWER_OFF);
+}
+
+char get_val() {
+    char result = 0;
+    char buffer[30];
+    receive_tram(buffer);
+    for(char i = 7; i<11; i++) {
+        result += ascii_hex_to_decimal(buffer[i]);
+        if(i!=10) result = result << 4; 
+    }
+    return result;
+}
+
+char get_voltage () {
+    send_tram(READ_VOLTAGE);
+    return get_val();
+}
+
+char get_current () {
+    send_tram(READ_CURRENT);
+    return get_val();
+}
+
+char get_power () {
+    send_tram(READ_POWER);
+    return get_val();
+}
 
 char get_count(char compteur)
 {      
@@ -115,6 +305,12 @@ void main(void)
     SSPCON = 0b00101000;
     SSPCON2 = 0b00000000;
     SSPADD = 24;
+    
+    RCSTA = RCSTA_byte;
+    TXSTA = TXSTA_byte;
+    SPBRG = SPBRG_byte;
+
+    set_power(35); // 35 W
     
     char nb_position = 0;
     char compteur,compteur2,compteur3;   // compteur pour les rubriques
@@ -359,204 +555,4 @@ void main(void)
         }
        afficheur(1,0);
     }         
-}
-#include "main.h"
-
-// PREFIXES
-#define PRE_SET_VALUE ":01060015"
-
-// ENABLE
-#define ENABLE_REMOTE_MODE ":010601000001F7"
-
-// CONFIGURE 
-#define ENABLE_VOLTAGE ":010600160000E3"
-#define ENABLE_CURRENT ":010600160010D3"
-#define ENABLE_POWER ":010600160020C3"
-
-// READING
-#define READ_VOLTAGE ":010300100001EB"
-#define READ_CURRENT ":010300110001EA"
-#define READ_POWER ":010300120001E9"
-
-// COMMANDS
-#define POWER_ON ":010600170011D1"
-#define POWER_OFF ":010600170000E2"
-
-void to_upper (char * str) {
-  for (int i = 0; str[i]; i++) {
-    char c = str[i];
-    if (c >= 97 && c <= 122) {
-      c -= 32;
-    }
-    str[i] = c;
-  }
-}
-
-char ascii_hex_to_decimal(char letter) {
-    char num = 0;
-    num = num << 4;
-    num = (letter<='9')?(letter-'0'):(letter-'a'+10);
-    num = (num & 0b00001111);
-    return num;
-}
-
-void decimal_to_ascii_hex(int decimal, char * buffer) {
-    sprintf(buffer, "%x", decimal);
-}
-
-void send_tram(char * tram) {
-    while(*tram != '\0') {
-        while (!TXIF);
-        TXREG = *tram;
-        tram++;
-    }
-    while (!TXIF);
-    TXREG = '\r';
-    while (!TXIF);
-    TXREG = '\n';
-}
-
-void receive_tram(char * buffer) { 
-    RCSTAbits.CREN = 1;
-    if(OERR) {
-        //If error, set bit of reading to 1.
-        RCSTAbits.CREN = 0;
-        RCSTAbits.CREN = 1;
-    }
-    
-    while(!RCIF);
-    while(RCREG != '\r') {
-        //Wait until the reception buffer is empty.
-        while(!RCIF);
-        *buffer = RCREG;
-        buffer++;
-    }
-}
-
-int chksum_calculation (const char * chain, char * buffer) {
-  int i, j, chksum = 0;
-  char chksum_str[3] = "";
-
-  strcpy(buffer, chain);
-  
-  for (i = 1; chain[i]; i+=2) {
-    int s = ascii_hex_to_decimal(chain[i]) * 16 + ascii_hex_to_decimal(chain[i+1]);
-    chksum += s;
-  }
-
-  printf("%d %x\n", chksum, chksum);
-  chksum = (chksum & 0b11111111);
-
-  decimal_to_ascii_hex(256-chksum, chksum_str);
-  strcat(buffer, chksum_str);
-
-  return chksum;
-}
-
-void set_value (char tram[30], int val) {
-    char asciihex[10] = "";
-    sprintf(asciihex, "%04x", val);
-    strcat(tram, asciihex);
-}
-
-void set_voltage (int voltage) {
-    // Enable remote mode
-    // Send UART
-    send_tram(ENABLE_REMOTE_MODE);
-    // Enable voltage mode
-    // Send UART
-    send_tram(ENABLE_VOLTAGE);
-    // Set V value
-    char new_tram3[30] = "";
-    char tram[30] = PRE_SET_VALUE;
-    set_value(tram, voltage);
-    // Checksum
-    chksum_calculation(tram, new_tram3);
-    to_upper(new_tram3);
-    // Send UART
-    send_tram(new_tram3);
-    printf("%s\n%s\n%s\n", ENABLE_REMOTE_MODE, ENABLE_VOLTAGE, new_tram3);
-}
-
-void set_current (int current) {
-    // Enable remote mode
-    // Send UART
-    send_tram(ENABLE_REMOTE_MODE);
-    // Enable current mode
-    // Send UART
-    send_tram(ENABLE_CURRENT);
-    // Set A value
-    char new_tram3[30] = "";
-    char tram[30] = PRE_SET_VALUE;
-    set_value(tram, current);
-    // Checksum
-    chksum_calculation(tram, new_tram3);
-    to_upper(new_tram3);
-    // Send UART
-    send_tram(new_tram3);
-    printf("%s\n%s\n%s\n", ENABLE_REMOTE_MODE, ENABLE_CURRENT, new_tram3);
-}
-
-void set_power (int power) {
-    // Enable remote mode
-    // Send UART
-    send_tram(ENABLE_REMOTE_MODE);
-    // Enable power mode
-    // Send UART
-    send_tram(ENABLE_POWER);
-    // Set W value
-    char new_tram3[30] = "";
-    char tram[30] = PRE_SET_VALUE;
-    set_value(tram, power);
-    // Checksum
-    chksum_calculation(tram, new_tram3);
-    to_upper(new_tram3);
-    // Send UART
-    send_tram(new_tram3);
-    printf("%s\n%s\n%s\n", ENABLE_REMOTE_MODE, ENABLE_POWER, new_tram3);
-}
-
-void turn_power_on (void) {
-  send_tram(POWER_ON);
-}
-
-void turn_power_off (void) {
-  send_tram(POWER_OFF);
-}
-
-char get_val() {
-    char result = 0;
-    char buffer[30];
-    receive_tram(buffer);
-    for(char i = 7; i<11; i++) {
-        result += ascii_hex_to_decimal(buffer[i]);
-        if(i!=10) result = result << 4; 
-    }
-    return result;
-}
-
-char get_voltage () {
-    send_tram(READ_VOLTAGE);
-    return get_val();
-}
-
-char get_current () {
-    send_tram(READ_CURRENT);
-    return get_val();
-}
-
-char get_power () {
-    send_tram(READ_POWER);
-    return get_val();
-}
-
-int main(void) {
-    // Please put the config in the main !!!
-
-    RCSTA = RCSTA_byte;
-    TXSTA = TXSTA_byte;
-    SPBRG = SPBRG_byte;
-
-    set_power(35); // 35 W
-    return (0);
 }
